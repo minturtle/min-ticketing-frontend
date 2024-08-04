dfd54600b5c9d5679ea16a23f6eac8592aef7645
minturtle <minseok0425@kakao.com>
2024-08-04 23:19:16 +0900

    feat: Header Component 및 CSS 개발

---
+++ src/components/Header.js
@@ -0,0 +1,21 @@
+import "./Header.css"
+
+function Header() {
+    return (
+        <header>
+            <div class="container header-content">
+                <a href="#" class="logo">MinTicketing</a>
+                <nav>
+                    <ul>
+                        <li><a href="#">홈</a></li>
+                        <li><a href="#">로그인/회원가입</a></li>
+                        <li><a href="#">마이페이지</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </header>
+    )
+
+}
+
+export default Header
