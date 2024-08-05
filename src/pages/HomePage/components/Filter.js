42a529a1dc29389f0d678a1e5d312ebca1042e02
minturtle <minseok0425@kakao.com>
2024-08-05 22:47:42 +0900

    fix : 메인 페이지 CSS 수정

--- src/pages/HomePage/components/Filter.js
+++ src/pages/HomePage/components/Filter.js
@@ -6,17 +6,19 @@ function Filter() {
             <h2>공연 찾기</h2>
             <form>
                 <div>
-                    <label for="date"><i class="far fa-calendar-alt"></i> 공연 날짜</label>
+                    <label for="date"><i class="far fa-calendar-alt"></i> 공연 날짜</label><br />
                     <input type="date" id="date" name="date" />
                 </div>
-                <div class="price-range">
-                    <label><i class="fas fa-tag"></i> 가격 범위</label>
-                    <input type="number" id="price-min" name="price-min" class="price-input" placeholder="최소 금액" min="0" />
-                    <span>~</span>
-                    <input type="number" id="price-max" name="price-max" class="price-input" placeholder="최대 금액" min="0" />
+                <div>
+                    <label><i class="fas fa-tag"></i> 가격 범위</label><br />
+                    <div class="price-input-container">
+                        <input type="number" id="price-min" name="price-min" class="price-input" placeholder="최소 금액" min="0" />
+                        <span>~</span>
+                        <input type="number" id="price-max" name="price-max" class="price-input" placeholder="최대 금액" min="0" />
+                    </div>
                 </div>
                 <div>
-                    <label for="region"><i class="fas fa-map-marker-alt"></i> 지역</label>
+                    <label for="region"><i class="fas fa-map-marker-alt"></i> 지역</label><br />
                     <select id="region" name="region">
                         <option value="">전체</option>
                         <option value="seoul">서울</option>
@@ -24,7 +26,7 @@ function Filter() {
                         <option value="daegu">대구</option>
                     </select>
                 </div>
-                <button type="submit">검색</button>
+                <button type="submit" className="searchButton">검색</button>
             </form>
         </section>
 
